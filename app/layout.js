b2a4bda2938ff6763037ee6b9848397715c66053
BestarDev <superstar9772@outlook.com>
2024-06-05 19:53:57 +0700

    Create Register Page

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="vh-100 d-flex flex-column">
         <TopNav />
         {children}
       </body>
---
+++ app/register/page.js
@@ -0,0 +1,16 @@
+
+
+export default function Register() {
+
+    return (
+        <main className="h-100">
+            <div className="container h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-lg-5 shadow bg-light p-5">
+                        <h2 className="mb-4 text-center">Register</h2>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
